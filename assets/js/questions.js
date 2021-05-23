f44f622b24ac214e892b2b3c3001e4b012fe22d2
McHenry <lukemchenry311@gmail.com>
2021-05-23 09:00:47 -0600

    first commit, skeleton of document

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// list of all questions 
+var questions = [
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    },
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    },
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    },
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    },
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    },
+    {
+        title: "are you an ugly moron",
+        choices: ["yea", "lol", "while", "do-while loops"],
+        answer: "yea"
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start');
+
+// Timer that counts down from 10
+function countdown() {
+    var timeLeft = 10;
+    
+    // Use the setInterval() method to call this function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function() {
+        // as long as the "timeLeft" is greater than 1 
+        if (timeLeft > 1) {
+            // set the 'textContent' of 'timerEl' to show the remaining seconds
+            timerEl.textContent = timeLeft + ' seconds remaining ';
+            // Decrement 'timeLeft' by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // When 'timeLeft' is equal to 1, rename to second' instead of 'seconds'
+            timerEl.textContent = timeLeft + 'second remaining';
+            timeLeft--;
+        } else {
+            // once 'timeLeft' gets to 0, set 'timerEl' to an empty string
+            timerEl.textContent = '';
+            // Use 'clearInterval() to stop the timer
+            clearInterval(timeInterval);
+            // Call the 'displayMessage()' function
+            displayMessage();
+        }
+    }, 1000);
+}
+startBtn.onclick = countdown;
