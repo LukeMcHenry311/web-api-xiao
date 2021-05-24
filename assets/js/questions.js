57c07059c01ae0b73247262903d42645c9e2e5c1
McHenry <lukemchenry311@gmail.com>
2021-05-23 22:56:01 -0600

    updated questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,48 @@
 // list of all questions 
 var questions = [
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
+        title: "Which HTML element do you put javascript",
+        choices: ["<javascript>", "<script>", "<scripture>", "<js>"],
+        answer: "<script>"
     },
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
+        title: "where is the correct place to insert Javascript",
+        choices: ["The <head> section", "The <body> section", "Both the <head> section and the <body> section are correct",],
+        answer: "Both the <head> section and the <body> section are correct"
     },
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
+        title: "Which event occurs when the user click on an HTML element",
+        choices: ["onclick", "onmouseover", "onchange", "onmouseclick"],
+        answer: "onclick"
     },
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
+        title: "How do you round the number 7.25, to the nearest integer",
+        choices: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        answer: "Math.round(7.25)"
     },
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
+        title: "JavaScript is the same as Java",
+        choices: ["False", "True"],
+        answer: "False"
     },
     {
-        title: "are you an ugly moron",
-        choices: ["yea", "lol", "while", "do-while loops"],
-        answer: "yea"
-    }
+        title: "Which operater is used to assign a value to a variable",
+        choices: ["*", "=", "+", "x"],
+        answer: "="
+    },
+    {
+        title: "how to insert a comment",
+        choices: ["// comment comment comment", "<!-- comment comment comment -->", "/* comment comment comment */"],
+        answer: "// comment comment comment"
+    },
+    {
+        title: "how do you create a function in javascript",
+        choices: [
+            "function (a) {",
+            "a => {",
+            "var myFunction (p2, p2) {",
+            "all of the above would create a function"
+        ],
+        answer: "all of the above would create a function"
+    },
 ];
